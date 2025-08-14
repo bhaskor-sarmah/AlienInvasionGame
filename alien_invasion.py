026e2d6a6fa0793905dd56271ffccdd43be3f11a
Bhaskor Jyoti Sarmah <bhaskor88@gmail.com>
2025-08-14 19:16:32 +0530

    Initial Commit

---
+++ alien_invasion.py
@@ -0,0 +1,52 @@
+import sys
+
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.settings = Settings()
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((self.settings.screen_width, 
+                                               self.settings.screen_height))
+        self.ship = Ship(self)
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self._update_screen()
+            # Trying to make the while loop run 60 times per second
+            # Pygame will try its best to maintain this frame rate
+            self.clock.tick(60)
+    
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 10
+                elif event.key == pygame.K_LEFT:
+                    self.ship.rect.x -= 10
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
